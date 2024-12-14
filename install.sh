66eb3c450c94411a75d877047a56e62e7bdc76cb
Иван <82300276+fatyzzz@users.noreply.github.com>
2024-12-14 19:16:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -114,13 +114,13 @@ install_byedpi() {
 
 # Загрузка и обработка списков
 fetch_configuration_lists() {
-    local setup_repo="https://github.com/fatyzzz/Byedpi-Setup/archive/refs/heads/main.zip"
-    local setup_zip="$TEMP_DIR/Byedpi-Setup-main.zip"
+    local setup_repo="https://github.com/fatyzzz/Byedpi-Setup/archive/refs/heads/dev.zip"
+    local setup_zip="$TEMP_DIR/Byedpi-Setup-dev.zip"
 
     safe_download "$setup_repo" "$setup_zip"
     unzip -q "$setup_zip" -d "$TEMP_DIR"
 
-    cd "$TEMP_DIR/Byedpi-Setup-main/assets" || exit 1
+    cd "$TEMP_DIR/Byedpi-Setup-dev/assets" || exit 1
 
     pip install requests
     python3 link_get.py
